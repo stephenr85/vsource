3b15ee82e7f6f443e79ffa1245b83f8f25d63c17
Stephen Rushing <stephen.rushing@esiteful.com>
2017-09-14 17:01:58 -0400

    Latest code for pulling Sales Contacts via One to One API

--- _config.php
+++ _config.php
@@ -12,7 +12,7 @@ define('VSOURCE_LOCALE', 'en_US');
 define('VSOURCE_LUMSITES_AUTH_CONFIG', 'private/client_secret_861547954802-5fso73p0o2mp18frr8keg1jjmarkoddc.apps.googleusercontent.com.json');
 define('VSOURCE_LUMSITES_AUTH_SCOPES', 'https://www.googleapis.com/auth/userinfo.email');
 define('VSOURCE_LUMSITES_AUTH_SUBJECT', 'us.vna.vsource.mailbox@veolia.com');
-define('VSOURCE_LUMSITES_AUTH_REFRESH', '1/N8q7bq3aINVYG9i_wP68GU7HBsdw_R79hDykAeEIQoY');
+define('VSOURCE_LUMSITES_AUTH_REFRESH', '1/CujC6y9bekaXz-_yRQRsybnAueJBUMmBXLH0YuNfUCQ');
 
 if($_SERVER['HTTP_HOST'] == 'vsource.local'){
 	define('VSOURCE_VIEW_ROOT', '/');
--- views/en_US/sales_contacts.php
+++ views/en_US/sales_contacts.php
@@ -13,330 +13,51 @@
     <div class="row">
   
   <div class="col-xs-12">
-  
+  <!--
     <p>Looking for Sales Contacts?<br>
-You can find them here.</p>
+You can find them here.</p>-->
     
 
   <?php 
-    /*$feed = new \Vsource\SalesContactsFeed();
-    $feedItems = $feed->loadOriginalData();
-    print_r($feedItems['template']['components']);die;
+    $feed = new \Vsource\SalesContactsFeed();
+    //$feedItems = $feed->loadOriginalData();
+    $feedData = $feed->loadData();
+    //print_r($feedData);die;
 
-    foreach($feedItems['template']['components'] as $components){
-      foreach($components as $component){
-        if($component['widgetType'] == 'about'){
-          echo '<h2>'.$component['properties']['title']['en'].'</h2>';
-          echo '<p>'.$component['properties']['content']['en'].'</p>';
-        }else if($component['widgetType'] == 'html'){
-          echo '<div>'..'</div>';
-        }
+    $print_contact = function($contact) {
+      $html = '<p>';
+      $html .= '<strong>' . $contact->name . '</strong>';
+      if($contact->email) $html .= '<br><a href="mailto:'.$contact->email.'">'.$contact->email.'</a>';
+      if($contact->phone) $html .= '<br><a href="tel:'.$contact->phone.'">'.$contact->phone.'</a>';
+      if($contact->description) $html .= '<br>'.$contact->description;
+      $html .= '</p>';
+
+      return $html;
+    };
+
+    $print_category = function($cat) use (&$print_category, &$print_contact){
+      $html = '<div data-role="collapsible">';
+      $html .= '<h2 class="redbutton" style="background-color: '.$cat->color.' !important;">'.$cat->name.'</h2>';
+
+      $html .= '<p>'.$cat->description.'</p>';
+
+      foreach($cat->contacts as $contact){
+        $html .= $print_contact($contact);
       }
-    }*/
+      foreach($cat->children as $child){
+        $html .= $print_category($child);
+      }
+
+      $html .= '</div>';
+      return $html;
+    };
+
+    foreach($feedData as $cat){
+      echo $print_category($cat);
+    }
+
   ?>
 
-    <!-- ... -->
-
-    <div data-role="collapsibleset" data-inset="false">
-    <div data-role="collapsible">
-<h2 class="redbutton">M&amp;C- National</h2>
-
-<p>  The M&amp;C business works in more than 550 communities in North America, managing large and complex water and energy infrastructure.  We increase efficiency and protect valuable resources, allowing our customers to focus on their core business.</p>
-<p> <strong>Mark Sanderson</strong><br>
-  <a href="mailto:Mark.sanderson@veolia.com">mark.sanderson@veolia.com</a><br>
-  <a href="tel:719.332.4184">719.332.4184</a><br>
-  Mark leads M&amp;C business development, particularly focusing on major contracts with large cities.</p>
-<p><strong>Patrick Schultz</strong><br>
-  <a href="mailto:Patrick.schultz@veolia.com">patrick.schultz@veolia.com</a><br>
-  <a href="tel:917.680.9201">917.680.9201</a><br>
-Patrick leads business development for the Peer Performance Solutions consulting business, where Veolia works side by side with utilities to identify efficiencies and operational improvements.</p>
-<p><strong>Mike Byrnes</strong><br>
-  <a href="mailto:mbyrnes@s1inc.com">mbyrnes@s1inc.com</a><br>
-  <a href="tel:212.612.7621">212.612.7621</a><br>
-  Mike is in charge of Veolia's solutions business, which includes S1, Enovity, and PPS</p>
-<p> <strong>Lou Schoen</strong><br>
-  <a href="mailto:lschoen@s1inc.com">lschoen@s1inc.com</a><br>
-  <a href="tel:212.612.7616">212.612.7616</a><br>
-  Lou leads business development for the energy consulting side of the business.</p>
-<p> <strong>Bill Naughton </strong><br>
-  <a href="mailto:bill.naughton@veolia.com">bill.naughton@veolia.com</a><br>
-  <a href="tel:617.356.4636">617.356.4636</a><br>
-  Bill leads business development for the federal market.</p>
-</div>
-
-<div data-role="collapsible">
-<h2 class="greenbutton">M&amp;C Regional BD Leads</h2>
-<div data-role="collapsibleset" data-inset="false">
-    <div data-role="collapsible"><h3>West Region</h3>
-<p><strong>David Schneider</strong><br>
-  <a href="mailto:David.schneider@veolia.com">david.schneider@veolia.com</a><br>
-  <a href="tel:323.217.4348">323.217.4348</a><br>
-David leads business development efforts for municipal water and energy contracts in the West Region, including British Columbia.</p>
-<p><strong>Colin Vorse</strong><br>
-  <a href="mailto:cvorse@s1inc.com">cvorse@s1inc.com</a><br>
-  <a href="tel:802.318.3471">802.318.3471</a><br>
-Colin leads business development on the west coast for energy consulting services.</p>
-
-</div>
-
-<div data-role="collapsible"><h3>Central Region</h3>
-<p><strong>Suresh Jambunathan</strong><br>
-  <a href="mailto:Suresh.jambunathan@veolia.com">suresh.jambunathan@veolia.com</a><br>
-  <a href="tel:630.335.4544">630.335.4544</a><br>
-Suresh leads business development efforts for municipal water and energy contracts in the Central Region, which includes Central Canada and Quebec.</p>
-<p><strong>Matt Duffy</strong><br>
-  <a href="mailto:matthew.duffy@veolia.com">matthew.duffy@veolia.com</a><br>
-  <a href="tel:832.300.5708">832.300.5708</a><br>
-Matt supports the Central region BD team. Matt is based in Houston and focuses on utility operations for industrial customers, as well as M&amp;C prospects in the region.</p>
-<p> <strong>Patrick Meyers</strong><br>
-  <a href="mailto:patrick.meyers@veolia.com">patrick.meyers@veolia.com</a><br>
-  <a href="tel:832.472.8362">832.472.8362</a><br>
-  Pat supports business development efforts in the M&amp;C Central region</p>
-</div>
-
-<div data-role="collapsible"><h3>South Region</h3>
-<p><strong>Jeff Brown</strong><br>
-  <a href="mailto:jeffrey.brown@veolia.com">jeffrey.brown@veolia.com</a><br>
-  <a href="tel:443.280.1900">443.280.1900</a><br>
-Jeff leads business development in the South region, particularly focusing on off-pipe energy O&amp;M.</p>
-</div>
-
-<div data-role="collapsible"><h3>Northeast/Mid-Atlantic Region</h3>
-<p><strong>Tom McCarthy</strong><br>
-  <a href="mailto:Thomas.mccarthy@veolia.com">thomas.mccarthy@veolia.com </a><br>
-  <a href="tel:603.715.7797">603.715.7797</a><br>
-Tom leads BD efforts for energy projects in the Northeast and Mid-Atlantic.</p>
-<p><strong>Farzin Kiani</strong><br>
-  <a href="mailto:Farzin.kiani@veolia.com">farzin.kiani@veolia.com </a><br>
-  <a href="tel:857.401.9873">857.401.9873</a><br>
-Farzin leads BD efforts for municipal water contracts in the Northeast and Mid-Atlantic</p>
-<p> <strong>Paul Skinner</strong><br>
-  <a href="mailto:paul.skinner@veolia.com">paul.skinner@veolia.com</a><br>
-  <a href="tel:508.441.9490">508.441.9490</a><br>
-Paul supports business development efforts focused on energy operations in the Northeast and Mid-Atlantic.</p>
-</div>
-</div>
-
-</div>
-
-<div data-role="collapsible">
-<h2 class="bluebutton">ESS</h2>
-<p>Our Environmental Services &amp; Solutions (ESS) business manages hazardous and non-hazardous waste for customers, keeping potentially harmful substances out of the environment and reusing precious natural resources.<br>
-</p>
-<p> <strong>Rob Wheatley</strong><br>
-  <a href="mailto:robert.wheatley@veolia.com">robert.wheatley@veolia.com</a><br>
-  <a href="tel:313.268.3457">313.268.3457</a><br>
-Rob leads all national sales efforts for the ESS group and Industrial Canada. The contacts below manage regional sales and report up to Rob.</p>
-
-<div data-role="collapsibleset" data-inset="false">
-  <div data-role="collapsible">
-<h3>Canada East (Quebec)</h3>
-<p><strong>Vincent Le Corre</strong><br>
-  <a href="mailto:Vincent.lecorre@veolia.com">vincent.lecorre@veolia.com </a><br>
-<a href="tel:514.348.1899">514.348.1899</a></p>
-</div>
-<div data-role="collapsible">
-<h3>Canada West (everywhere but Quebec)</h3>
-<p><strong>Peter Hitchen</strong><br>
-  <a href="mailto:Peter.hitchen@veolia.com">peter.hitchen@veolia.com </a><br>
-<a href="tel:905.543.9295">905.543.9295</a></p>
-</div>
-<div data-role="collapsible">
-<h3>ERD (Electronics Recycling Division)</h3>
-<p><strong>Fred Ribelli</strong><br>
-  <a href="mailto:Fred.ribelli@veolia.com">fred.ribelli@veolia.com </a><br>
-  <a href="tel:262.243.8900">262.243.8900</a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>New England Branch</h3>
-<p><strong>Jane McCarthy</strong><br>
-  <a href="mailto:Jane.mccarthy2@veolia.com">jane.mccarthy2@veolia.com </a><br>
-  <a href="tel:508.561.5897">508.561.5897</a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>New Jersey Branch</h3>
-<p><strong>Peter Maraziti</strong><br>
-  <a href="mailto:Peter.maraziti@veolia.com">peter.maraziti@veolia.com </a><br>
-  973.691.7359<br></p>
-</div>
-<div data-role="collapsible">
-<h3>Mid-Atlantic Branch</h3>
-<p><strong>Karen Ockford</strong><br>
-  <a href="mailto:Karen.ockford@veolia.com">karen.ockford@veolia.com </a><br>
-  <a href="tel:215.537.7305">215.537.7305</a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>Ohio Valley Branch</h3>
-<p><strong>Bob Luzanski</strong><br>
-  <a href="mailto:Bob.luzanski@veolia.com">bob.luzanski@veolia.com </a><br>
-  <a href="tel:724.872.4825">724.872.4825</a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>Midwest Branch</h3>
-<p><strong>Rob Huisinga</strong><br>
-  <a href="mailto:Rob.huisinga@veolia.com">rob.huisinga@veolia.com </a><br>
-  <a href="tel:618.271.2804">618.271.2804</a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>Central Branch</h3>
-<p><strong>John Mueller</strong><br>
-  <a href="mailto:john.mueller@veolia.com">john.mueller@veolia.com</a><br>
-  <a href="tel:262.224.8152">262.224.8152 </a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>Gulf Coast Branch</h3>
-<p><strong>Carson Jarnagin</strong><br>
-  <a href="mailto:Carson.jarnagin@veolia.com">carson.jarnagin@veolia.com </a><br>
-  <a href="tel:281.425.7154">281.425.7154</a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>Mountain Branch</h3>
-<p><strong>Derek Nelhuebel</strong><br>
-  <a href="mailto:Derek.nelhuebel@veolia.com">derek.nelhuebel@veolia.com </a><br>
-  <a href="tel:303.885.2882">303.885.2882</a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>West Branch</h3>
-<p><strong>Greg Giandomenico</strong><br>
-  <a href="mailto:Greg.giandomenico@veolia.com">greg.giandomenico@veolia.com </a><br>
-  <a href="tel:626.536.9561">626.536.9561</a><br></p>
-</div>
-</div>
-</div>
-
-<div data-role="collapsible">
-<h2 class="redbutton">Industrial Cleaning</h2>
-<p>Our Industrial Cleaning solutions return productivity and uptime to our customers, helping reduce their total cost of ownership. Through our portfolio of Hands Free™ cleaning technologies, we are working safer, cleaner, and faster than ever before.</p>
-<p> <strong>Kevin Zenkevich</strong><br>
-  <a href="mailto:kevin.zenkevich@veolia.com">kevin.zenkevich@veolia.com</a><br>
-Kevin leads national sales for the Industrial Cleaning group. The contacts below manage regional efforts and report up to Kevin.</p>
-
-<div data-role="collapsible">
-<h3>North Region</h3>
-<p> <strong>Tara Pote</strong><br>
-  <a href="mailto:tara.pote@veolia.com">tara.pote@veolia.com</a><br>
-  <a href="tel:815.302.0783">815.302.0783</a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>South East Region</h3>
-<p><strong>Troy Williams </strong><br>
-  <a href="mailto:Troy.williams@veolia.com">troy.williams@veolia.com </a><br>
-  <a href="tel:985.652.0187">985.652.0187</a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>South Region, Gulf Coast</h3>
-<p> <strong>Jody DeLuca</strong><br>
-  <a href="mailto:Jody.deluca@veolia.com">jody.deluca@veolia.com </a><br>
-  <a href="tel:281.739.1695">281.739.1695</a><br></p>
-</div>
-
-<div data-role="collapsible">
-<h3>East Region</h3>
-<p> <strong>Dave Vetter</strong><br>
-  <a href="mailto:David.vetter@veolia.com">David.vetter@veolia.com </a><br>
-  <a href="tel:606.644.0426">606.644.0426</a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>South West Region</h3>
-<p><strong>Eddie Gerren</strong><br>
-  <a href="mailto:eddie.gerren@veolia.com">eddie.gerren@veolia.com</a><br>
-  <a href="tel:512.760.0330">512.760.0330</a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>Resource Solutions</h3>
-<p><strong>Michelle McIntyre </strong><br>
-  <a href="mailto:michelle.mcintyre@veolia.com">michelle.mcintyre@veolia.com</a><br>
-  <a href="tel:248.697.0136">248.697.0136</a><br>
-  <a href="tel:248.755.9026">248.755.9026 (cell)</a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>National Chemical Cleaning</h3>
-<p><strong>John Beale </strong><br>
-  <a href="mailto:john.beale@veolia.com">john.beale@veolia.com</a><br>
-  <a href="tel:714.262.6517">714.262.6517</a><br></p>
-</div>
-
-<div data-role="collapsible">
-<h3>National Tank &amp; Separations</h3>
-<p><strong>Al Flath 
-  </strong><br>
-  <a href="mailto:al.flath@veolia.com">al.flath@veolia.com </a><br>
-  <a href="tel:606.831.9233">606.831.9233 </a><br></p>
-</div>
-
-<div data-role="collapsible">
-<h3>National Turnaround Services</h3>
-<p><strong>Doug Theriot</strong><br>
-  <a href="mailto:doug.theriot@veolia.com">doug.theriot@veolia.com</a><br>
-  <a href="tel:337.499.9345">337.499.9345 </a><br></p>
-</div>
-</div>
-
-<div data-role="collapsible">
-<h2 class="greenbutton">Industrial Water &amp; Regeneration Services</h2>
-<p>Water is a critical component of nearly all industrial processes, which is why more than 100 industrial complexes trust Veolia to manage their water and wastewater operations. We help our customers optimize and improve the reliability of their vital water resources and systems - protecting the environment and the bottom line.</p>
-<div data-role="collapsible">
-<h3>Major DBOM (Design, Build, Operate and Maintain), Industrial Water</h3>
-<p> <strong>John Brinker</strong><br>
-  <a href="mailto:John.brinker@veolia.com">john.brinker@veolia.com </a><br>
-  <a href="tel:412.298.1808">412.298.1808</a></p>
-</div>
-<div data-role="collapsible">
-<h3>Oily Residuals Management</h3>
-<p><strong>Scott Ortego</strong><br>
-  <a href="mailto:scott.ortego@veolia.com">scott.ortego@veolia.com </a><br></p>
-</div>
-<div data-role="collapsible">
-<h3>Regeneration Sales</h3>
-<p><strong>Mike Daly</strong><br>
-  <a href="mailto:michael.daly@veolia.com">michael.daly@veolia.com </a><br>
-  <a href="tel:610.229.5661">610.229.5661</a><br></p>
-</div>
-
-<div data-role="collapsible">
-<h3>Strategic Development &amp; Pricing</h3>
-<p><strong>Calvin LeFevre
-  </strong><br>
-  <a href="mailto:calivin.lefevre@veolia.com">calivin.lefevre@veolia.com </a><br>
-  <a href="tel:713.305.2933">713.305.2933 </a><br></p>
-</div>
-
-<div data-role="collapsible">
-<h3>Upstream Oil &amp; Gas</h3>
-<p><strong>Lnsp (Naggs) Nagghappan</strong><br>
-  <a href="mailto:naggs@veolia.com">naggs@veolia.com</a><br>
-  <a href="tel:412.720.9600">412.720.9600</a><br></p>
-</div>
-
-</div>
-<div data-role="collapsible">
-<h2 class="bluebutton">MarCom Contacts</h2>
-<p>Still not sure who to contact? Reach out to the appropriate MarCom contact and we will point you in the right direction.</p>
-<div data-role="collapsibleset" data-inset="false">
-    <div data-role="collapsible">
-<h3>M&amp;C</h3>
-<p><strong>Dan Seiden</strong><br>
-  <a href="mailto:Dseiden@s1inc.com">dseiden@s1inc.com</a><br>
-<a href="tel:212.612.7645">212.612.7645</a></p>
-</div>
-    <div data-role="collapsible">
-<h3>ESS</h3>
-<p><strong>Jim Dykhuis</strong><br>
-  <a href="mailto:Jim.dykhuis@veolia.com">jim.dykhuis@veolia.com</a><br>
-<a href="tel:630.218.1763">630.218.1763</a></p>
-</div>
-    <div data-role="collapsible">
-<h3>IND Cleaning and IND Water and Regeneration Services</h3>
-<p><strong>Harrison Lee</strong><br>
-  <a href="mailto:harrison.lee@veolia.com">harrison.lee@veolia.com</a><br>
-</p>
-</div>
-</div>
-</div>
-</div>
-
-
-    <!-- ... -->
   
   </div>
 
